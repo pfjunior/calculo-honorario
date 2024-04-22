049fd00989587f3410d782497a0964ee3ab9c7c1
pfjunior <pf.junior89@hotmail.com>
2024-04-21 21:06:25 -0300

    adicionando resposta padrao da api, criando os dtos

--- CalculoHonorario.Api/Endpoints/HomeEndpoint.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CalculoHonorario.Api.Endpoints;
-
-public static class HomeEndpoint
-{
-    public static void MapHomeEndpoint(this WebApplication app) => app.MapGet("/", () => Results.Ok("Teste"));
-}
